4b341b0042b65f87ad707b477093008b0eeb7233
Omar Eltantawy <110872105+omar01140@users.noreply.github.com>
2025-05-28 18:48:12 +0300

    Implement timer functionality in HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, signal } from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
@@ -9,5 +9,36 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy {
+  StartBtn = signal(true)
+  minutes = signal('00')
+  hours = signal('00')
+  interval: any = null;
+  startTime: number = 0;
+  elapsedTime: number = 0;
+
+  start(){
+    if (this.interval) return;
+    this.StartBtn.set(!this.StartBtn());
+
+    this.startTime = Date.now() - this.elapsedTime;
+    this.interval = setInterval(() => {
+      this.elapsedTime = Date.now() - this.startTime;
+      const totalSeconds = Math.floor(this.elapsedTime / 1000);
+      this.hours.set(String(Math.floor(totalSeconds / 3600)).padStart(2, '0'))
+      this.minutes.set(String(Math.floor((totalSeconds % 3600) / 60)).padStart(2, '0'))
+      console.log(totalSeconds)
+    }, 1000);
+  }
+  end(){
+    this.StartBtn.set(!this.StartBtn());
+    this.hours.set('00')
+    this.minutes.set('00')
+    this.elapsedTime = 0;
+    clearInterval(this.interval)
+    this.interval = null;
+  }
+    ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
 }
