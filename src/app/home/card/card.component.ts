3bf4945eeabd9a9530672bf02d6d2abc9f13b4d4
Omar Eltantawy <110872105+omar01140@users.noreply.github.com>
2025-05-29 21:38:50 +0300

    separate card component

---
+++ src/app/home/card/card.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnDestroy, signal } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [MatMenuModule, MatSlideToggleModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+    StartBtn = signal(true)
+  minutes = signal('00')
+  hours = signal('00')
+  interval: any = null;
+  startTime: number = 0;
+  elapsedTime: number = 0;
+
+  start(){
+    if (this.interval) return;
+    this.StartBtn.set(!this.StartBtn());
+
+    this.startTime = Date.now() - this.elapsedTime;
+    this.interval = setInterval(() => {
+      this.elapsedTime = Date.now() - this.startTime;
+      const totalSeconds = Math.floor(this.elapsedTime / 1000);
+      this.hours.set(String(Math.floor(totalSeconds / 3600)).padStart(2, '0'))
+      this.minutes.set(String(Math.floor((totalSeconds % 3600) / 60)).padStart(2, '0'))
+      console.log(totalSeconds)
+    }, 1000);
+  }
+  end(){
+    this.StartBtn.set(!this.StartBtn());
+    this.hours.set('00')
+    this.minutes.set('00')
+    this.elapsedTime = 0;
+    clearInterval(this.interval)
+    this.interval = null;
+  }
+    ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+}
